fec91b666b19e1625e0649df737ee2b110161fdc
Shivam Shashank <31573704+ricku44@users.noreply.github.com>
2018-06-24 02:31:14 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,20 @@
 // python -m http.server
 // http://localhost:8000/kissanime.js
 
+// CONFIG
+var siteName = "Kissanime"
+var rootUrl = 'http://ww7.kiss-anime.me/'
 var URL = window.location.origin
+// END CONFIG
+
 
 var episodeLinks = $('table.listing a').map(function(i,el) { return $(el).attr('href'); });
 var episodeNames = $('table.listing a').map(function(i,el) { return $.trim( $(el).html() ); });
 
 $.ajaxSetup({async:false});
-$.getScript("http://ww7.kiss-anime.me/Scripts/asp.js");
+$.getScript(rootUrl + "/Scripts/asp.js");
 
-console.log('Starting Kissanime Batch Downloader script...');
+console.log('Starting ' + siteName + ' Batch Downloader script...');
 
 var startEpisode;
 do {
@@ -40,6 +45,15 @@ if (videoQuality == null){
 	videoQuality = "720,480,360";
 }
 
+var opOptions = prompt(
+	"How do you want output to be?\n0 = simple list of links\n1 = List with filenames (for wget, aria2 helper scripts)\n2 = HTML page with links",
+	defaultText="0"
+);
+
+if (opOptions == null){
+	opOptions = "0";
+}
+
 var i;
 var linkStr = "";
 
@@ -59,6 +73,15 @@ for (i = (episodeLinks.length - startEpisode); i >= (episodeLinks.length - endEp
 
 			var data = $(result).find("#divDownload");  // download data
 			var links = $(data[0]).find("a");
+
+			if (data == null || data == "" || data.length == 0){ // captcha maybe
+				console.log("Captcha detected at " + URL + episodeLinks[i]);
+				prompt("Captcha detected. Solve it by opening the link below in a new tab. After solving, press OK.",
+					defaultText=URL + episodeLinks[i]);
+				this.tryCount++;
+				$.ajax(this);  // retry
+				return;
+			}
 			// console.log(links);
 			
 			var quals = videoQuality.split(',');
@@ -73,7 +96,14 @@ for (i = (episodeLinks.length - startEpisode); i >= (episodeLinks.length - endEp
 					// console.log(el);
 					if ( $(el).html().search(quals[j]) > -1 ){
 						long_url = $(el).attr('href');
-						linkStr += long_url + "\n";
+						name = getDownloadName(episodeNames[i], $(el).html());
+						if (opOptions == "1"){
+							linkStr += encodeURI(long_url) + " " + name + "\n";
+						} else if (opOptions == "2"){
+							linkStr += '<a href="' + long_url + '" download="' + name + '">' + name + '</a><br>';
+						} else {
+							linkStr += long_url + "\n";
+						}
 						found = true;
 						// console.log('Episode ' + (episodeLinks.length - i));
 						console.log(long_url);
@@ -99,12 +129,12 @@ for (i = (episodeLinks.length - startEpisode); i >= (episodeLinks.length - endEp
 }
 
 console.log('Opening list of links')
-download("links.txt", linkStr)
+download("links." + ((opOptions == '2') ? 'html' : 'txt'), (opOptions == '2') ? 'text/html' : 'text/plain', linkStr)
 
 // http://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
-function download(filename, text) {
+function download(filename, datatype, text) {
 	var element = document.createElement('a');
-	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+	element.setAttribute('href', 'data:' + datatype + ';charset=utf-8,' + encodeURIComponent(text));
 	element.setAttribute('download', filename);
 	// element.setAttribute('target', '_blank');
 	// ^^ problems with safari
@@ -115,4 +145,8 @@ function download(filename, text) {
 	element.click();
 
 	document.body.removeChild(element);
+}
+
+function getDownloadName(epName, dl){
+	return (epName + "__" + dl).replace(/\s/g, '_');
 }
